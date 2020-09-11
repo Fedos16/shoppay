aea64d6f6b8979e7cab54dcc48e5233539aa1712
olegator072@gmail.com <Asser220_>
2020-09-11 16:05:12 +0300

    1

---
+++ config.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root('.env') });
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    //LOGIN_SBER: "marusik-api",
+    //PASSWORD_SBER: "Summer2018!",
+    //TOKEN_SBER: "ers1jrg6kuqu216cjn5m94ar1b",
+    LOGIN_SBER: "marusik-11001-api",
+    PASSWORD_SBER: "xZtA79hBB",
+    TOKEN_SBER: "vqo693o0vkivb1719k2r81cq3",
+    MONGO_URL: 'mongodb://fedos:asser220@ds024778.mlab.com:24778/shopping',  
+    IS_PRODUCTION: process.env.NODE_ENV === 'production',
+    DESTINATION: 'uploads',
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    S3_BUCKET: process.env.S3_BUCKET
+  };
